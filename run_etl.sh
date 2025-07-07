703b7d30f0a7f13555568c809e15e5cb1e8b27ba
persian-123 <rohamrahimi22@gmail.com>
2025-07-07 11:01:30 +0100

    dusud

--- run_etl.sh
+++ run_etl.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Check if python3 and required packages are installed
-command -v python3 >/dev/null 2>&1 || { echo "Python3 is required but not installed. Aborting."; exit 1; }
-
 # Install required packages if not already installed
 pip_install() {
   package=$1
