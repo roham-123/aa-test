9aa74e8d456d223e0ea619587bc9af5b7d40d99f
persian-123 <rohamrahimi22@gmail.com>
2025-07-08 15:53:39 +0100

    untracking db_config

--- db_config.py
+++
@@ -1,8 +0,0 @@
-# Central place for database configuration so other modules can import it
-
-DB_CONFIG = {
-    "host": "localhost",
-    "user": "root",
-    "password": "Roham137789+",
-    "database": "aa_poll_demo",
-} 
